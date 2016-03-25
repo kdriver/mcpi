5f670263cff52c671325fd348ce4d582d36ae610
kdriver <keith@just-the-one.co.uk>
2016-03-25 09:53:35 +0000

    added castle

--- bob.py
+++ bob.py
@@ -2,6 +2,7 @@ from mcpi import minecraft
 from mcpi import block
 from house import *
 from cottage import *
+from castle import *
 
 mc = minecraft.Minecraft.create()
 
@@ -15,20 +16,19 @@ mc.setBlocks(-127,0,-127,127,50,127,air)
 
 mc.setBlocks(-127,-1,-127,127,-1,127,stone)
 
-mc.postToChat("Bob The Builder")
+mc.postToChat("Bob The Fixer")
 x=0
 y=0
 z=0
-mc.player.setPos(x,y,z)
+mc.player.setPos(0,0,0)
 
 build_house(mc) 
 
 
 
-mc.player.setPos(x-25,y,z)
+mc.player.setPos(-25,0,0)
 build_cottage(mc)
 
-
-
-
+mc.player.setPos(-50,0,0)
+build_castle(mc,-50,0,0)
 
--- cottage.py
+++ cottage.py
@@ -5,7 +5,7 @@ def build_cottage(mc):
         
         z=z+5
         
-        mc.postToChat("Build a cottage")
+        mc.postToChat("fixed the cottage")
         air=block.AIR.id
         stone=block.STONE.id
         brick=block.BRICK_BLOCK.id
@@ -19,11 +19,29 @@ def build_cottage(mc):
         oak_plank=block.WOOD_PLANKS.id
         wool=block.WOOL.id
         yellow_wool=4
+        orange_wool=1
+        brown_wool=12
         ladder=block.LADDER.id
+        bed=block.BED.id
+        bookcase=block.BOOKSHELF.id
+        painting=321
+        furnace=62
+        craft=block.CRAFTING_TABLE.id
+        stonebrick=block.STONE_BRICK.id
+        cobblestone=block.COBBLESTONE.id
+        stoneslab_double=block.STONE_SLAB_DOUBLE.id
+        ironblock=block.IRON_BLOCK.id
+        irondoor=block.DOOR_IRON.id
 
+        south=0
+        west=1
+        north=2
+        east=3
+        
         width=6
         length=8
-        height=5
+        height=4
+    
 
       
 
@@ -38,7 +56,7 @@ def build_cottage(mc):
         mc.setBlocks(x,y-1,z,x+length,y-1,z+width,oak_plank)
         
         # put a hole in for the ladder
-        mc.setBlocks(x+length-2,y,z+width-1,x+length-1,y+(height/2),z+width-1,air)
+        mc.setBlocks(x+length-2,y+2,z+width-2,x+length-2,y+2,z+width-2,air)
 
         #put the roof on
         mc.setBlocks(x,y+height+1,z,x+length,y+height+1,z+width,wool,yellow_wool)
@@ -57,10 +75,74 @@ def build_cottage(mc):
 ##        mc.setBlock(x+1,y+(height/5)-1,z+width-3,torch)
 
         # put the ladder in
-        mc.setBlock(x+length-4,y,z+width-1,ladder)
+        mc.setBlocks(x+length-1,y+2,z+width-1,x+length-1,y+2,z+width-1,ladder)
+        mc.setBlocks(x+length-1,y+1,z+width-1,x+length-1,y+1,z+width-1,ladder)
+        mc.setBlocks(x+length-1,y,z+width-1,x+length-1,y,z+width-1,ladder)
+        mc.setBlocks(x+length-1,y+3,z+width-1,x+length-1,y+3,z+width-1,ladder)
+        mc.setBlocks(x+length-1,y+4,z+width-1,x+length-1,y+4,z+width-1,ladder)
+        
 
-         # put the windows in
+        # put the windows in
         mc.setBlocks(x,y,z+(width/3),x,y+1,z+width-(width/3),glass)
+        # put the second floor in
+        mc.setBlocks(x,y+2,z,x+length,y+2,z+width,oak_plank)
+        mc.setBlocks(x+length-1,y+2,z+width-1,x+length-1,y+2,z+width-1,air)
+
+
+        # put bed in
+        mc.setBlocks(x,y+3,z+3,x,y+3,bed)
+
+        # put glowstone in right
+        mc.setBlocks(x,y+3,z+1,x,y+3,z+1,glowstone)
+        mc.setBlocks(x,y+4,z+1,x,y+4,z+1,glowstone)
+
+        # put glowstone in left
+        mc.setBlocks(x,y+3,z+5,x,y+3,z+5,glowstone)
+        mc.setBlocks(x,y+4,z+5,x,y+4,z+5,glowstone)
+
+        # put glowstone in right other
+        mc.setBlocks(x+8,y+3,z+1,x+8,y+3,z+1,glowstone)
+        mc.setBlocks(x+8,y+4,z+1,x+8,y+4,z+1,glowstone)
+
+        # put glowstone in right other
+        mc.setBlocks(x+8,y+3,z+5,x+8,y+3,z+5,glowstone)
+        mc.setBlocks(x+8,y+4,z+5,x+8,y+4,z+5,glowstone)
+
+        # put bookcases by the  side of the bed
+        mc.setBlocks(x+1,y+3,z+2,x+1,y+3,z+2,bookcase)
+        mc.setBlocks(x+1,y+3,z+4,x+1,y+3,z+4,bookcase)
+        mc.setBlocks(x+1,y+4,z+2,x+1,y+4,z+2,bookcase)
+        mc.setBlocks(x+1,y+4,z+4,x+1,y+4,z+4,bookcase)
+
+        # put painting on the wall
+        mc.setBlocks(x+3,y+4,z+1,x+3,y+4,z+1,painting,2)
+        
+        # put a furnace in
+        mc.setBlocks(x+1,y,z+5,x+1,y,z+5,furnace,east)
+
+        # put the stone blocks in as the counter tops
+        mc.setBlocks(x+1,y,z+4,x+1,y,z+4,stoneslab_double)
+        mc.setBlocks(x+1,y,z+2,x+1,y,z+2,stoneslab_double)
+
+        # put a crafting table in
+        mc.setBlocks(x+1,y,z+3,x+1,y,z+3,craft)
+
+        # put a fridge in
+        mc.setBlocks(x+1,y,z+1,x+1,y,z+1,ironblock)
+        mc.setBlocks(x+1,y+1,z+1,x+1,y+1,z+1,ironblock)
+        mc.setBlocks(x+2,y,z+1,x+2,y,z+1,door)
+        mc.setBlocks(x+2,y+1,z+1,x+2,y+1,z+1,door)
+
+        # put a sofa dowstairs
+        mc.setBlocks(x+7,y,z+1,x+7,y,z+1,wool,orange_wool)
+        mc.setBlocks(x+7,y,z+2,x+7,y,z+2,stoneslab)
+        mc.setBlocks(x+7,y,z+3,x+7,y,z+3,stoneslab)
+        mc.setBlocks(x+7,y,z+4,x+7,y,z+4,wool,orange_wool)
+
+
+
+
+        
         return
        
 
